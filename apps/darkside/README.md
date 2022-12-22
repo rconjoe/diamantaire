57df28885c3946ce7d4fa9b5a951381e4cc3a67a
rconjoe <joe@rconjoe.com>
2022-12-22 07:09:38 -0800

    AUTOMATEDupdate-rollup-executor

---
+++ apps/darkside/README.md
@@ -0,0 +1,3 @@
+# VRAI.com Micro Frontend Architecture (MFE)
+
+### _built with [Next.js Multi Zones](https://nextjs.org/docs/advanced-features/multi-zones)_
--- apps/darkside/shell/next.config.js
+++ apps/darkside/shell/next.config.js
@@ -1,5 +1,7 @@
 //@ts-check
 
+const { JEWELRY_URL } = process.env;
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
@@ -11,6 +13,26 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/jewelry/:path*',
+        destination: `${JEWELRY_URL}/jewelry/:path*`,
+      },
+      // {
+      //   source: '/blog',
+      //   destination: `${BLOG_URL}/blog`,
+      // },
+      // {
+      //   source: '/blog/:path*',
+      //   destination: `${BLOG_URL}/blog/:path*`,
+      // },
+    ];
   },
   i18n: {
     locales: ['en-US', 'fr', 'de', 'es'],
