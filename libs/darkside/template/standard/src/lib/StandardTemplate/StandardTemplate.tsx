faf97ce4d1b07a55bea7717188c5bfd435e6ea41
rconjoe <joe@rconjoe.com>
2023-01-04 12:30:52 -0800

    this query should not be here??

--- libs/darkside/template/standard/src/lib/StandardTemplate/StandardTemplate.tsx
+++ libs/darkside/template/standard/src/lib/StandardTemplate/StandardTemplate.tsx
@@ -7,57 +7,9 @@ export interface StandardTemplateProps {
   children: ReactNode;
 }
 
-export const HEADER_NAV_QUERY = `
-query headerNavigationDynamicQuery($locale: SiteLocale) {
-    headerNavigationDynamic(locale: $locale) {
-      section {
-        title
-        key
-        route
-        columns {
-          columnTitle
-          route
-          colKey
-          links {
-            _modelApiKey
-            id
-            copy
-            route
-            isBold
-            linkKey
-            flags
-            nestedLinks {
-              id
-              copy
-              route
-              isBold
-            }
-            supportedCountries {
-              code
-            }
-          }
-        }
-      }
-      mobileAccordionOrder {
-        key
-        label
-        isCollapsible
-      }
-      mobileLocalizationAccordionOrder {
-        key
-        isCollapsible
-        label
-      }
-      iconsAltText {
-        key
-        value
-      }
-    }
-  }
-`;
-
 export function StandardTemplate({ children }: StandardTemplateProps) {
   const [thing, setThing] = useState('default-state');
+  const;
 
   return (
     <div className={styles['container']}>
