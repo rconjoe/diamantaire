bc9d7d7eecfac79485cb1f4dccbc9d899b505618
rconjoe <joe@rconjoe.com>
2023-01-04 12:26:40 -0800

    (wip) update react-query implementation

--- libs/darkside/core/src/lib/custom-app/CustomApp.tsx
+++ libs/darkside/core/src/lib/custom-app/CustomApp.tsx
@@ -1,9 +1,14 @@
-import type { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useState } from 'react';
 import type { NextPage } from 'next';
 import { AppProps } from 'next/app';
 
-import { ReactQueryProvider } from '@diamantaire/darkside/data/react-query';
-import { DehydratedState } from 'react-query';
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export type PageComponentWithTemplate<
   P = Record<string, unknown>,
@@ -20,9 +25,18 @@ export type AppPropsWithTemplate = AppProps & {
 export function CustomApp({ Component, pageProps }: AppPropsWithTemplate) {
   const getTemplate = Component.getTemplate ?? ((page) => page);
 
+  // https://tanstack.com/query/v4/docs/guides/ssr#using-hydration
+  // Layout files can then use useQuery() hooks to call for their data by key.
+  // As long as the same keys are prefetched in the GSSP/SSG of the page using that layout,
+  // the data will always be there at first render.
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
-      {getTemplate(<Component {...pageProps} />)}
-    </ReactQueryProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        {getTemplate(<Component {...pageProps} />)}
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
