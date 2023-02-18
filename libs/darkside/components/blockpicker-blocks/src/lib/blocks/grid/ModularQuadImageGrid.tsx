3acd96ae6b8a5abb297ac55de8feca9ca7f72e0b
Rico Velasco <rico.velasco@vrai.com>
2023-02-17 16:25:42 -0800

    fix(shared/styles): entry file config, set as .ts:wq

--- libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularQuadImageGrid.tsx
+++ libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularQuadImageGrid.tsx
@@ -1,3 +1,29 @@
+/** This section is a 2x2 image grid on desktop, and a centered slider on mobile */
+/** This section is a 2x2 image grid on desktop, and a centered slider on mobile */
+
+// type ModularGridCarouselBlockProps = {
+//   title?: string;
+//   subtitle?: string;
+//   blocks?: Array<any>;
+//   additionalClass?: string;
+//   id?: string;
+//   isMobile?: boolean;
+//   headingAdditionalClass?: string;
+//   headingType?: string;
+//   countryCode: string;
+//   shouldLazyLoad?: boolean;
+// };
+
+// blocks,
+// additionalClass,
+// title,
+// subtitle,
+// headingType,
+// headingAdditionalClass,
+// isMobile,
+// shouldLazyLoad,
+// countryCode,
+
 const ModularQuadImageGrid = () => {
   return <h1>temp</h1>;
 };
