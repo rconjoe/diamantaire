44a6982bbfab4200229fe955edbc1060e1833f41
div <samjdavidoff@gmail.com>
2023-03-28 18:08:47 -0400

    feat(rebase develop): rebase develop

--- libs/shared/icons/src/index.ts
+++ libs/shared/icons/src/index.ts
@@ -1,3 +1,4 @@
+// Removing this import breaks our ability to export images https://github.com/nrwl/nx/issues/12008#issuecomment-1256616788
 import './lib/icons.d';
 
 export * from './lib/diamondShapes';
