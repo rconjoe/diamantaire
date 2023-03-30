2a57c8f2fd069932e755f059fab55b1e2bf7219c
div <samjdavidoff@gmail.com>
2023-03-30 10:27:55 -0400

    feat: rebase develop

--- libs/darkside/page/standard-pages/src/lib/StandardPageEntry.tsx
+++ libs/darkside/page/standard-pages/src/lib/StandardPageEntry.tsx
@@ -23,7 +23,7 @@ const StandardPageEntry = ({ page, isMobile, countryCode, currencyCode }: Standa
   return (
     <div className="content-one-container">
       <Suspense fallback={'Loading'}>
-        {page?.content1?.slice(0, 9).map((contentBlockData, idx) => {
+        {page?.content1?.map((contentBlockData, idx) => {
           const { id, _modelApiKey } = contentBlockData;
 
           // Desktop + Mobile, anything after the first two blocks should be lazy loaded
