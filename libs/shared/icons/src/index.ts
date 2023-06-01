5c151ad2a0bdc3cddbe5f9c26216b5f75ae0b1b7
div <samjdavidoff@gmail.com>
2023-06-01 09:48:50 -0400

    fix: fix icon type declaration

--- libs/shared/icons/src/index.ts
+++ libs/shared/icons/src/index.ts
@@ -1,5 +1,6 @@
 // Removing this import breaks our ability to export images/svgs https://github.com/nrwl/nx/issues/12008#issuecomment-1256616788
-// import './lib/icons.d';
+
+//// <reference path="./lib/icons.d.ts" />
 
 export * from './lib/diamondShapes';
 export * from './lib/ringStyles';
