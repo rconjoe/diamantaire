3038cc12c17c8f6411c0351e35b95374e8267f96
div <samjdavidoff@gmail.com>
2023-06-01 09:23:43 -0400

    feat: scaffold basic pdp

--- libs/shared/icons/src/index.ts
+++ libs/shared/icons/src/index.ts
@@ -1,5 +1,5 @@
 // Removing this import breaks our ability to export images/svgs https://github.com/nrwl/nx/issues/12008#issuecomment-1256616788
-import './lib/icons.d';
+// import './lib/icons.d';
 
 export * from './lib/diamondShapes';
 export * from './lib/ringStyles';
