04e3c5fc493bb54bb70dad69c88f63f028502fa3
Rico Velasco <rico.velasco@vrai.com>
2023-08-17 08:52:26 -0700

    feat(diamond plp): use constant for video path, created helper lotId mutation function

--- libs/darkside/components/common-ui/src/lib/molecules/DiamondVideoThumbImage.tsx
+++ libs/darkside/components/common-ui/src/lib/molecules/DiamondVideoThumbImage.tsx
@@ -1,3 +1,5 @@
+import { DIAMOND_VIDEO_BASE_URL } from '@diamantaire/shared/constants';
+import { getNumericalLotId } from '@diamantaire/shared-diamond';
 import Image from 'next/image';
 
 type DiamondVideoThumbImageProps = {
@@ -9,8 +11,8 @@ type DiamondVideoThumbImageProps = {
 };
 
 const DiamondVideoThumbImage = ({ lotId, alt, className, width = 400, height = 400 }: DiamondVideoThumbImageProps) => {
-  const mutatedLotId = lotId.replace(/F/g, '');
-  const src = `https://videos.diamondfoundry.com/${mutatedLotId}-thumb.jpg`;
+  const numericalLotId = getNumericalLotId(lotId);
+  const src = `${DIAMOND_VIDEO_BASE_URL}/${numericalLotId}-thumb.jpg`;
 
   return <Image src={src} alt={alt} width={width} height={height} sizes="100%" className={className} />;
 };
