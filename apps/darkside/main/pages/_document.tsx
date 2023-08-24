19db99afce0589df689a89f6b734e048d7dcbd9f
Dan Au <buzzguy@gmail.com>
2023-08-24 10:05:37 -0400

    update to new v3 sdk

--- apps/darkside/main/pages/_document.tsx
+++ apps/darkside/main/pages/_document.tsx
@@ -29,8 +29,20 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <Script id="rudderstack-js" strategy="beforeInteractive">
-            {`!function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],e.factory=function(t){return function(){e.push([t].concat(Array.prototype.slice.call(arguments)))}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.rudderlabs.com/v1.1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
-e.load('2UAYRK0ZoTfyotdORlAATyZugQ3', 'https://dfvodevspwfsas.dataplane.rudderstack.com')}();`}
+            {`!function(){"use strict";var sdkBaseUrl="https://cdn.rudderlabs.com/beta/3.0.0-beta";var sdkName="rsa.min.js"
+;var asyncScript=true;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[]
+;var e=["setDefaultInstanceKey","load","ready","page","track","identify","alias","group","reset","setAnonymousId","startSession","endSession"]
+;for(var n=0;n<e.length;n++){var d=e[n];window.rudderanalytics[d]=function(e){return function(){
+window.rudderanalytics.push([e].concat(Array.prototype.slice.call(arguments)))}}(d)}try{
+new Function('return import("")'),window.rudderAnalyticsBuildType="modern"}catch(a){}
+if(window.rudderAnalyticsMount=function(){var e=document.createElement("script")
+;e.src="".concat(sdkBaseUrl,"/").concat(window.rudderAnalyticsBuildType,"/").concat(sdkName),e.async=asyncScript,
+document.head?document.head.appendChild(e):document.body.appendChild(e)},"undefined"==typeof Promise){
+var t=document.createElement("script")
+;t.src="https://polyfill.io/v3/polyfill.min.js?features=globalThis%2CPromise&callback=rudderAnalyticsMount",
+t.async=asyncScript,document.head?document.head.appendChild(t):document.body.appendChild(t)}else{
+window.rudderAnalyticsMount()}window.rudderanalytics.load("2UAYRK0ZoTfyotdORlAATyZugQ3","https://dfvodevspwfsas.dataplane.rudderstack.com",{})}();
+`}
           </Script>
         </Head>
 
