6bca09fd894d475a5b6bd7c609f8fd71c6019438
Joe Carlton <jcarlton@vrai.com>
2023-09-08 10:04:22 -0700

    refactor(darthfeeder): remove accidental fake error logging 400 (left in scratch code) and add comments

--- apps/darthfeeder/app/api/health/route.ts
+++ apps/darthfeeder/app/api/health/route.ts
@@ -2,6 +2,7 @@ import { HealthEndpoint } from '../../../lib/endpoints/health';
 import { makeNextAppHandler } from '../../../lib/handlers';
 import { makeOkResult } from '../../../lib/util';
 
+// TODO: can use this to check whatever
 const handlers = makeNextAppHandler(HealthEndpoint, {
   async GET() {
     return makeOkResult({
