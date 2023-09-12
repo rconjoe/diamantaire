fdfcfaac56973abe3ea02518585beadf6ad1f886
Dan Au <buzzguy@gmail.com>
2023-09-12 15:35:20 -0400

    fix import order

--- libs/darkside/core/src/lib/custom-app/CustomApp.tsx
+++ libs/darkside/core/src/lib/custom-app/CustomApp.tsx
@@ -1,9 +1,6 @@
 import { DefaultSeo } from '@diamantaire/darkside/components/seo';
-
 import { AnalyticsProvider } from '@diamantaire/darkside/context/analytics';
-
 import { CartProvider } from '@diamantaire/darkside/context/cart-context';
-
 import { GlobalProvider } from '@diamantaire/darkside/context/global-context';
 import { BuilderProductContextProvider } from '@diamantaire/darkside/context/product-builder';
 import { GlobalStyles } from '@diamantaire/styles/darkside-styles';
@@ -41,15 +38,15 @@ export function CustomApp({ Component, pageProps }: AppPropsWithTemplate) {
         <GlobalProvider>
           <PageLoadProgressBar />
           <BuilderProductContextProvider>
-          <CartProvider>
-            <DefaultSeo />
-            <GlobalStyles />
-            <Hydrate state={pageProps.dehydratedState}>
-              {getTemplate(<Component {...pageProps} key={router.asPath} />)}
-            </Hydrate>
-            <ReactQueryDevtools initialIsOpen={false} />
-          </CartProvider>
-            </BuilderProductContextProvider>
+            <CartProvider>
+              <DefaultSeo />
+              <GlobalStyles />
+              <Hydrate state={pageProps.dehydratedState}>
+                {getTemplate(<Component {...pageProps} key={router.asPath} />)}
+              </Hydrate>
+              <ReactQueryDevtools initialIsOpen={false} />
+            </CartProvider>
+          </BuilderProductContextProvider>
         </GlobalProvider>
       </AnalyticsProvider>
     </QueryClientProvider>
