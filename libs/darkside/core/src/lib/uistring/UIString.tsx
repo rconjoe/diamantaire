93ac4bc66ed93a305e79040f79831cd1b9d2aada
Rico Velasco <rico.velasco@vrai.com>
2023-09-26 20:24:16 -0700

    fix: ts

--- libs/darkside/core/src/lib/uistring/UIString.tsx
+++ libs/darkside/core/src/lib/uistring/UIString.tsx
@@ -3,15 +3,15 @@ import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 type UIStringProps = {
-  children: string;
-  replacements?: (string | ReactNode)[];
+  children: string | string[];
+  replacements?: ReactNode[];
 };
 
 const UIString = ({ children, replacements }: UIStringProps) => {
   const router = useRouter();
   const { _t } = useTranslations(router.locale);
 
-  return _t(children, replacements);
+  return _t(children.toString(), replacements);
 };
 
 export { UIString };
