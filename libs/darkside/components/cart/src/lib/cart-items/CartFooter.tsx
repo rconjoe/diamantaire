3b22337f40c8d59b908d80bad050d83a6d8fc7dd
Dan Au <buzzguy@gmail.com>
2023-09-29 13:15:53 -0400

    move feedid down

--- libs/darkside/components/cart/src/lib/cart-items/CartFooter.tsx
+++ libs/darkside/components/cart/src/lib/cart-items/CartFooter.tsx
@@ -241,8 +241,8 @@ const CartFooter = ({ checkout, checkoutCta, termsCta, termsCtaLink }: CartFoote
           ),
         },
         items,
-        feedIds: (items || []).map(({ item_id }) => item_id),
       },
+      feedIds: (items || []).map(({ item_id }) => item_id),
     };
 
     checkoutStarted(eventData);
