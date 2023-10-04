2c24bed87577d9158e159790197e8869d632ed91
Rico Velasco <rico.velasco@vrai.com>
2023-10-03 19:00:06 -0700

    fix: rename middleware exported fn

--- apps/darkside/main/middleware.ts
+++ apps/darkside/main/middleware.ts
@@ -2,7 +2,7 @@ import { kv } from '@vercel/kv';
 import { NextMiddlewareResult } from 'next/dist/server/web/types';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export async function darksideMiddleware(
+export async function middleware(
   request: NextRequest,
   response: NextResponse,
   _event: NextFetchEvent,
