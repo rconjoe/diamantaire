7b816d3a424a8ae96a5c199b2d4a4ca603272b6a
charles olivier <chla84@yahoo.fr>
2023-10-12 14:48:25 +0200

    feat: country code cookie

--- libs/darkside/core/src/lib/custom-app/CustomApp.tsx
+++ libs/darkside/core/src/lib/custom-app/CustomApp.tsx
@@ -4,18 +4,22 @@ import { AnalyticsProvider } from '@diamantaire/darkside/context/analytics';
 import { CartProvider } from '@diamantaire/darkside/context/cart-context';
 import { GlobalProvider } from '@diamantaire/darkside/context/global-context';
 import { BuilderProductContextProvider } from '@diamantaire/darkside/context/product-builder';
+import { getCountry } from '@diamantaire/shared/helpers';
 import { GlobalStyles } from '@diamantaire/styles/darkside-styles';
 import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { CookieConsentProvider } from '@use-cookie-consent/react';
+import Cookies from 'js-cookie';
 import type { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { ReactElement, ReactNode, useState } from 'react';
-import './styles.css';
 import { ToastContainer } from 'react-toastify';
+import './styles.css';
 
 import 'node_modules/react-toastify/dist/ReactToastify.css';
 import PageLoadProgressBar from '../progressbar/PageLoadProgressBar';
+
+import { useRouter } from 'next/router';
 
 export type PageComponentWithTemplate<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
   getTemplate?: (page: ReactElement) => ReactNode;
@@ -34,6 +38,12 @@ export function CustomApp({ Component, pageProps }: AppPropsWithTemplate) {
   // As long as the same keys are prefetched in the GSSP/SSG of the page using that layout,
   // the data will always be there at first render.
   const [queryClient] = useState(() => new QueryClient());
+
+  const router = useRouter();
+  const locale = router.locale;
+  const countryCode = getCountry(locale);
+
+  Cookies.set('geoCountry', countryCode);
 
   return (
     <QueryClientProvider client={queryClient}>
