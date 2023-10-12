2ce1fa96a44de94511fbd8ee80b7dc3672a511c8
Rico Velasco <rico.velasco@vrai.com>
2023-10-12 12:27:12 -0700

    feat: added bundle analyzer package

--- apps/darkside/main/next.config.js
+++ apps/darkside/main/next.config.js
@@ -3,8 +3,16 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+<<<<<<< HEAD
 const { withNx } = require('@nx/next/plugins/with-nx');
 const { withSentryConfig } = require('@sentry/nextjs');
+=======
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const { withNx, composePlugins } = require('@nx/next');
+// const { withSentryConfig } = require('@sentry/nextjs');
+>>>>>>> aac7a000 (feat: added bundle analyzer package)
 
 const DEFAULT_LOCALE = 'en-US';
 
@@ -134,25 +142,33 @@ const nextConfig = {
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
 
-  org: 'vrai-oro',
-  project: 'darkside-main',
+//   org: 'vrai-oro',
+//   project: 'darkside-main',
 
-  // An auth token is required for uploading source maps.
-  // You can get an auth token from https://sentry.io/settings/account/api/auth-tokens/
-  // The token must have `project:releases` and `org:read` scopes for uploading source maps
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+//   // An auth token is required for uploading source maps.
+//   // You can get an auth token from https://sentry.io/settings/account/api/auth-tokens/
+//   // The token must have `project:releases` and `org:read` scopes for uploading source maps
+//   authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  silent: true, // Suppresses all logs
+//   silent: true, // Suppresses all logs
 
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
+
+const plugins = [
+  withNx,
+  // (nextConfiguration) => withSentryConfig(nextConfiguration, sentryWebpackPluginOptions),
+  withBundleAnalyzer,
+];
 
 // FIXME: https://nx.dev/packages/next/documents/next-config-setup#composing-plugins-using-utility-(nx-16-and-later)
-module.exports = withNx(withSentryConfig(nextConfig, sentryWebpackPluginOptions));
+module.exports = composePlugins(...plugins)(nextConfig); // withNx(withSentryConfig(nextConfig, sentryWebpackPluginOptions));
+
+//module.exports = withNx(withBundleAnalyzer(nextConfig));
