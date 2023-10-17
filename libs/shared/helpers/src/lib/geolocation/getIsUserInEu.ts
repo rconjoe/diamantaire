8fee9783e60e24a39f2b4509255fd84a6656f736
charles olivier <chla84@yahoo.fr>
2023-10-17 14:13:22 +0200

    feat: getCountry

--- libs/shared/helpers/src/lib/geolocation/getIsUserInEu.ts
+++ libs/shared/helpers/src/lib/geolocation/getIsUserInEu.ts
@@ -1,6 +1,6 @@
 import { DEFAULT_LOCALE, countries } from '@diamantaire/shared/constants';
 
-import { getCountry } from './getCountry';
+import { getCountry } from '../getCountry';
 
 export const getIsUserInEu = (locale: string = DEFAULT_LOCALE) => {
   const countryCode = getCountry(locale);
