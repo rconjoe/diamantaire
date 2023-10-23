e806e9fb659bd53dd87ce0252cb277f390924d7d
Joe Carlton <jcarlton@vrai.com>
2023-10-22 18:48:38 -0700

    feat(nx-wmill): wip. finally arrived at a working way to handle child processes for now. effect packages versioned. zx and teen_proc added.

--- libs/nx/wmill/src/executors/sync/executor.ts
+++ libs/nx/wmill/src/executors/sync/executor.ts
@@ -1,8 +1,48 @@
-import { SyncExecutorSchema } from './schema';
+// import { spinner } from 'zx/experimental';
+// import { Effect, Context } from 'effect';
+import { exec } from 'teen_process';
+//
+// import { syncexecutorschema } from './schema';
+//
+// const synccontext = context.tag<syncexecutorschema>();
+//
+// class SyncError {
+//   _tag = 'SyncError';
+//   // prettier-ignore
+//   constructor(public message: string) { }
+// }
+//
+// const executeSync = Effect.gen(function*(_) {
+//   const ctx = yield* _(SyncContext);
+//
+//   console.log(ctx);
+//
+//   yield* _(
+//     Effect.tryPromise({
+//       try: () => $`wmill sync pull`,
+//       catch: () => new SyncError('Shit! `wmill sync pull` failed.'),
+//     }),
+//   );
+//
+//   return {
+//     success: true,
+//   };
+// });
 
 export default async function runExecutor(options: SyncExecutorSchema) {
-  console.log('Executor ran for Sync', options);
-  return {
-    success: true,
-  };
+  try {
+    const { stdout, stderr, code } = await exec('ls');
+
+    console.log(stdout.split('\n'));
+    console.log(stderr);
+    console.log(code);
+
+    return { success: true };
+  } catch (e) {
+    console.error(e);
+  }
+
+  // const runnable = Effect.provideService(executeSync, SyncContext, SyncContext.of(options));
+  //
+  // return Effect.runPromise(runnable);
 }
