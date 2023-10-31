94771c0b577b8bbdaf4e8d8cbe58b0074214a5d7
div <samjdavidoff@gmail.com>
2023-10-31 17:53:38 -0400

    fix import

--- libs/darkside/page/standard-pages/src/lib/StandardPageEntry.tsx
+++ libs/darkside/page/standard-pages/src/lib/StandardPageEntry.tsx
@@ -2,8 +2,9 @@
   This component implements the BlockPicker loop for the Home Page, and Standard Pages
 * */
 
+import { BlockPicker } from '@diamantaire/darkside/components/blockpicker-blocks';
 import clsx from 'clsx';
-// import React, { Suspense } from 'react';
+import React, { Suspense } from 'react';
 
 type StandardPageEntryProps = {
   page?: {
@@ -17,32 +18,44 @@ type StandardPageEntryProps = {
 
 const StandardPageEntry = ({ page, countryCode, currencyCode, gtmClass }: StandardPageEntryProps) => {
   // const { content1 } = page || [];
-  console.log(page, countryCode, currencyCode);
+  // console.log(page);
 
   return (
     <div className={clsx('content-one-container', gtmClass)}>
-      <h1>Are there savings</h1>
-      {/* <Suspense fallback={'Loading'}>
-        {page?.content1?.slice(0, page.content1.length).  map((contentBlockData, idx) => {
+      <Suspense fallback={'Loading'}>
+        {page?.content1?.slice(0, page.content1.length).map((contentBlockData, idx) => {
           const { _modelApiKey } = contentBlockData;
 
           // Desktop + Mobile, anything after the first two blocks should be lazy loaded
           const contentIsAboveFold = idx < 2;
           const shouldLazyLoad = contentIsAboveFold ? false : true;
 
-          return (
-            <React.Fragment key={`${_modelApiKey}_${idx}`}>
+          if (shouldLazyLoad) {
+            return (
+              <Suspense fallback={'Loading'} key={`${_modelApiKey}_${idx}`}>
+                <BlockPicker
+                  _modelApiKey={_modelApiKey}
+                  modularBlockData={{ ...contentBlockData }}
+                  countryCode={countryCode}
+                  currencyCode={currencyCode}
+                  shouldLazyLoad={shouldLazyLoad}
+                />
+              </Suspense>
+            );
+          } else {
+            return (
               <BlockPicker
                 _modelApiKey={_modelApiKey}
                 modularBlockData={{ ...contentBlockData }}
                 countryCode={countryCode}
                 currencyCode={currencyCode}
                 shouldLazyLoad={shouldLazyLoad}
+                key={`${_modelApiKey}_${idx}`}
               />
-            </React.Fragment>
-          );
+            );
+          }
         })}
-      </Suspense> */}
+      </Suspense>
     </div>
   );
 };
