19037fedb3cc4baf003817eec851c97f074989d8
Dan Au <buzzguy@gmail.com>
2023-11-20 14:47:36 -0500

    refactor isWithinInterval

--- libs/shared/helpers/src/lib/isCurrentTimeWithinInterval.ts
+++ libs/shared/helpers/src/lib/isCurrentTimeWithinInterval.ts
@@ -1,11 +1,29 @@
 import { isWithinInterval } from 'date-fns';
 
 export function isCurrentTimeWithinInterval(startTime: string, endTime: string): boolean {
-  const currentTime = new Date();
-  const startDateTime = new Date(startTime);
-  const endDateTime = new Date(endTime);
+  try {
+    const currentTime = new Date();
+    const startDateTime = new Date(startTime);
+    const endDateTime = new Date(endTime);
 
-  if (!currentTime || !startDateTime || !endDateTime) return false;
+    // Check if any of the dates are invalid
+    if (isNaN(startDateTime.getTime()) || isNaN(endDateTime.getTime())) {
+      console.error('Invalid start or end time');
 
-  return isWithinInterval(currentTime, { start: startDateTime, end: endDateTime });
+      return false;
+    }
+
+    // Check if end time is before start time
+    if (endDateTime < startDateTime) {
+      console.error('End time is before start time');
+
+      return false;
+    }
+
+    return isWithinInterval(currentTime, { start: startDateTime, end: endDateTime });
+  } catch (error) {
+    console.error('Error in isCurrentTimeWithinInterval:', error);
+
+    return false;
+  }
 }
