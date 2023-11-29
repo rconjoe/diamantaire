ec03826032cf52011e78da6451d29b745c4f7d5e
div <samjdavidoff@gmail.com>
2023-11-29 12:07:05 -0500

    remove log

--- libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularTrioBlock.tsx
+++ libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularTrioBlock.tsx
@@ -45,8 +45,6 @@ const ModularTrioBlock = (props: ModularTrioBlockProps) => {
   const { id, belowCopy, aboveCopy, headingType, headingAdditionalClass, blogPosts, blogPostCtaCopy, _modelApiKey } = props;
   const [trioBlocks, setTrioBlocks] = useState([]);
 
-  console.log('trio props', props);
-
   const [additionalClass, setAdditionalClass] = useState('');
 
   useEffect(() => {
