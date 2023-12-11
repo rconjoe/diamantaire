48cb89cb42a9916781a9110f45d8a54ffc547b1c
Dan Au <buzzguy@gmail.com>
2023-12-11 09:32:52 -0500

    refactor so gtm doesn't try to load while in dev mode w/o id

--- libs/shared/analytics/src/lib/GoogleTagManagerContainer.tsx
+++ libs/shared/analytics/src/lib/GoogleTagManagerContainer.tsx
@@ -3,25 +3,17 @@
 import { getIsUserInEu } from '@diamantaire/shared/geolocation';
 import { GoogleTagManager } from '@next/third-parties/google';
 import { useCookieConsentContext } from '@use-cookie-consent/react';
-import { useEffect } from 'react';
 
 const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID;
+const isDevEnabled = process.env.NEXT_PUBLIC_LOCAL_GTM === 'true';
 
 const GoogleTagManagerContainer = () => {
   const isUserInEu = getIsUserInEu();
   const { consent } = useCookieConsentContext();
 
-  const isEnabled = !isUserInEu || (consent?.statistics && consent?.marketing);
+  const isEnabled = !isUserInEu || (consent?.statistics && consent?.marketing) || isDevEnabled;
 
-  useEffect(() => {
-    if (!isEnabled || !GTM_CONTAINER_ID) {
-      return;
-    }
-
-    // Initialize Google Tag Manager
-  }, [isEnabled]);
-
-  if (!isEnabled) {
+  if (!isEnabled || !GTM_CONTAINER_ID) {
     return null;
   }
 
