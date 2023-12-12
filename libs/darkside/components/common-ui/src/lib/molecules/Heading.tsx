eb0fe4268f9897ff887a769da6d9258d50164a69
Dan Au <buzzguy@gmail.com>
2023-12-12 13:13:55 -0500

    change Heading back

--- libs/darkside/components/common-ui/src/lib/molecules/Heading.tsx
+++ libs/darkside/components/common-ui/src/lib/molecules/Heading.tsx
@@ -1,30 +1,21 @@
-import React from 'react';
+import { ReactNode } from 'react';
 
 type HeadingProps = {
-  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
-  children: React.ReactNode;
+  type?: string;
+  children: ReactNode;
   className?: string | 'primary' | 'secondary' | 'tertiary' | '-medium' | '-bold' | 'small';
 };
 
-const Heading: React.FC<HeadingProps> = ({ type = 'h1', children, className }) => {
-  const getHeading = (type: string) => {
-    switch (type) {
-      case 'h1':
-        return <h1 className={className}>{children}</h1>;
-      case 'h2':
-        return <h2 className={className}>{children}</h2>;
-      case 'h3':
-        return <h3 className={className}>{children}</h3>;
-      case 'h4':
-        return <h4 className={className}>{children}</h4>;
-      case 'h5':
-        return <h5 className={className}>{children}</h5>;
-      default:
-        return <h1 className={className}>{children}</h1>;
-    }
-  };
-
-  return getHeading(type);
+const Heading = ({ type, children, className }: HeadingProps) => {
+  return (
+    <>
+      {(type === 'h1' || !type) && <h1 className={className}>{children}</h1>}
+      {type === 'h2' && <h2 className={className}>{children}</h2>}
+      {type === 'h3' && <h3 className={className}>{children}</h3>}
+      {type === 'h4' && <h4 className={className}>{children}</h4>}
+      {type === 'h5' && <h5 className={className}>{children}</h5>}
+    </>
+  );
 };
 
 export { Heading };
