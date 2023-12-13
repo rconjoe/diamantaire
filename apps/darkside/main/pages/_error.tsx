5dcdfb5f53be7eaf7f95f73728fff972a8cffea0
Rico Velasco <rico.velasco@vrai.com>
2023-12-13 17:06:50 -0500

    added header to custom error page

--- apps/darkside/main/pages/_error.tsx
+++ apps/darkside/main/pages/_error.tsx
@@ -1,10 +1,13 @@
 import { DarksidePageError } from '@diamantaire/darkside/page/error';
+import { getTemplate as getStandardTemplate } from '@diamantaire/darkside/template/standard';
 import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
 const CustomErrorComponent = (props) => {
   return <DarksidePageError {...props} />;
 };
+
+CustomErrorComponent.getTemplate = getStandardTemplate;
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
   // In case this is running in a serverless function, await this in order to give Sentry
