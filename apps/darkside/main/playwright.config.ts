415cb1b197a73b275910b1e29a03f5adf92ff83d
chee.lim <128071093+clim0126@users.noreply.github.com>
2023-12-20 20:54:30 -0800

    commented out running webserver

--- apps/darkside/main/playwright.config.ts
+++ apps/darkside/main/playwright.config.ts
@@ -39,10 +39,10 @@ export default defineConfig({
     },
   ],
   /* Run your local dev server before starting the tests */
-   webServer: {
-     command: 'pnpm run start',
-     url: 'http://127.0.0.1:4200',
-     reuseExistingServer: !process.env.CI,
-     cwd: workspaceRoot,
-   },
+  //  webServer: {
+  //    command: 'pnpm run start',
+  //    url: 'http://127.0.0.1:4200',
+  //    reuseExistingServer: !process.env.CI,
+  //    cwd: workspaceRoot,
+  //  },
 });
