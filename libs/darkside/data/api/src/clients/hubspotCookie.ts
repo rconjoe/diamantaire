ddeaa9908df01ff12068d011f7fcb53bd93c6423
Dan Au <buzzguy@gmail.com>
2023-12-21 11:05:07 -0500

    set an account email cookie

--- libs/darkside/data/api/src/clients/hubspotCookie.ts
+++ libs/darkside/data/api/src/clients/hubspotCookie.ts
@@ -1,6 +1,7 @@
 import { decode, encode } from 'base-64';
 import Cookies from 'js-cookie';
 export const HUBSPOT_EMAIL_COOKIE = 'hbe';
+export const ACCOUNT_EMAIL_COOKIE = 'ae';
 
 export const hubspotEmailCookie = {
   set(val) {
@@ -13,5 +14,19 @@ export const hubspotEmailCookie = {
   },
   remove() {
     Cookies.remove(HUBSPOT_EMAIL_COOKIE);
+  },
+};
+
+export const accountEmailCookie = {
+  set(val) {
+    Cookies.set(ACCOUNT_EMAIL_COOKIE, encode(val), {
+      expires: 30,
+    });
+  },
+  get(cookies) {
+    return cookies[ACCOUNT_EMAIL_COOKIE] ? decode(cookies[ACCOUNT_EMAIL_COOKIE]) : false;
+  },
+  remove() {
+    Cookies.remove(ACCOUNT_EMAIL_COOKIE);
   },
 };
