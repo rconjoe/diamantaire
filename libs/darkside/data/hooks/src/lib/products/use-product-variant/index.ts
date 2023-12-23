630436e868d13ec01d1624bed3c27042f218089a
div <samjdavidoff@gmail.com>
2023-12-22 19:14:25 -0500

    log

--- libs/darkside/data/hooks/src/lib/products/use-product-variant/index.ts
+++ libs/darkside/data/hooks/src/lib/products/use-product-variant/index.ts
@@ -2,8 +2,6 @@ import { queries } from '@diamantaire/darkside/data/queries';
 import { useQuery } from '@tanstack/react-query';
 
 export function useProductVariant(variantSlugOrConfigurationVariantId: string, productType: string, locale: string) {
-  console.log('variantSlug', variantSlugOrConfigurationVariantId);
-
   return useQuery({
     ...queries.products.datoVariant(variantSlugOrConfigurationVariantId, productType, locale),
   });
