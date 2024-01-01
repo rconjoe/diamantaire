4f5eaa44ee3d3f8a8a3be2ed4aff1cfff7290332
cheevrai <152340773+cheevrai@users.noreply.github.com>
2023-12-31 18:43:19 -0800

    fix script error

--- apps/darkside/main/e2e/tests/fine-jewerly/potentialsmoke.ts
+++ apps/darkside/main/e2e/tests/fine-jewerly/potentialsmoke.ts
@@ -1,5 +1,4 @@
-import exp from "constants";
-import { test, expect } from "../../pages/page-fixture";
+import { test, } from "../../pages/page-fixture";
 
 /**
  * Fine Jewerly - basic flow - price validation
@@ -13,49 +12,49 @@ test.describe("Fine Jewerly Basic flow", () => {
       await homePage.open();
     });
 
-    // await test.step("select a setting", async () => {
-    //   await homePage.navigateToEngagementStartWithASetting();
-    // });
+    await test.step("select a setting", async () => {
+      await homePage.navigateToEngagementStartWithASetting();
+    });
 
-    // await test.step("Check All metal filter", async () => {
-    //   await settingPage.checkAllFilters();
-    // });
+    await test.step("Check All metal filter", async () => {
+      await settingPage.checkAllFilters();
+    });
 
-    // await test.step("select a setting", async () => {
-    //   await homePage.navigateToEngagementStartWithASetting();
-    // });
+    await test.step("select a setting", async () => {
+      await homePage.navigateToEngagementStartWithASetting();
+    });
 
-    // await test.step("Select Emarald", async () => {
+    await test.step("Select Emarald", async () => {
 
-    //   await settingPage.selectDiamondType('Emerald')
-    // });
+      await settingPage.selectDiamondType('Emerald')
+    });
 
-    // await test.step("Select Pave Cushion", async () => {
+    await test.step("Select Pave Cushion", async () => {
 
-    //   await settingPage.selectDiamondType('Pavé Cushion')
-    // });
+      await settingPage.selectDiamondType('Pavé Cushion')
+    });
 
-    // await test.step("Select setting", async () => {
+    await test.step("Select setting", async () => {
 
-    //   await settingPage.selectSettingBySettingName('Classic Hidden')
-    //  // await productDetailedPage.clickSelectYourVRAICreatedDiamond();
-    //   await productDetailedPage.clickAddToBag();
-    // });
+      await settingPage.selectSettingBySettingName('Classic Hidden')
+     // await productDetailedPage.clickSelectYourVRAICreatedDiamond();
+      await productDetailedPage.clickAddToBag();
+    });
 
-    // await test.step("Checkout slide", async () => {
-    //   await checkoutSlidePage.clickCheckout();
+    await test.step("Checkout slide", async () => {
+      await checkoutSlidePage.clickCheckout();
 
-    //   const element = await checkoutPage.page.getByText('Express Checkout');
+      const element = await checkoutPage.page.getByText('Express Checkout');
 
-    //   await expect(element !== undefined ).toBeTruthy();  
+      await expect(element !== undefined ).toBeTruthy();  
 
-    //   await checkoutPage.getCheckoutTotal();
-    // });
+      await checkoutPage.getCheckoutTotal();
+    });
 
-    // await test.step("Diamond Table", async () => {
-    //   await diamondTablePage.checkAllDiamondType()
+    await test.step("Diamond Table", async () => {
+      await diamondTablePage.checkAllDiamondType()
 
-    // });
+    });
 
     await test.step("select a diamond first", async () => {
       await homePage.navigateToEngagementStartWithAVRAICreatedDiamond();
