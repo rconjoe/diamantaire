fa9efc69d500b80eea52905a3ce193787bc3a27b
div <samjdavidoff@gmail.com>
2024-01-04 14:54:19 -0500

    override type

--- libs/darkside/components/blockpicker-blocks/src/lib/blocks/carousels/CarouselSlider.tsx
+++ libs/darkside/components/blockpicker-blocks/src/lib/blocks/carousels/CarouselSlider.tsx
@@ -2,7 +2,7 @@ import { DarksideButton, Heading } from '@diamantaire/darkside/components/common
 import { ArrowLeftIcon, ArrowRightIcon } from '@diamantaire/shared/icons';
 import { DatoDarksideButtonProps } from '@diamantaire/shared/types';
 import clsx from 'clsx';
-import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react';
+import useEmblaCarousel from 'embla-carousel-react';
 import { ReactNode } from 'react';
 
 import { CarouselSliderContainer } from './CarouselSlider.style';
@@ -32,7 +32,7 @@ const CarouselSlider = ({
   darksideButtons,
   _modelApiKey,
 }: CarouselContentBlockProps) => {
-  const sliderOptions: EmblaOptionsType = {
+  const sliderOptions: any = {
     loop: loopItems,
     dragFree: false,
     align: 'center',
