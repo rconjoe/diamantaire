87e487067802be50c5d614a085fc467e672135e1
charles olivier <chla84@yahoo.fr>
2024-01-12 15:00:24 +0100

    feat: diamond 360 animation

--- libs/darkside/components/diamonds/src/lib/Diamond360.tsx
+++ libs/darkside/components/diamonds/src/lib/Diamond360.tsx
@@ -98,7 +98,7 @@ const Diamond360 = ({
 
       {!disabled && !useImageOnly && (
         <AnimatePresence>
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2 }}>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
             <div className="vid">{vid}</div>
           </motion.div>
         </AnimatePresence>
