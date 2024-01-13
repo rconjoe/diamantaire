df25eac69d2a9bcf9f3a3af7e0ef2328c54f202d
Rico Velasco <rico.velasco@vrai.com>
2024-01-12 19:14:43 -0800

    setup logic to handle PDP redirects

--- apps/darkside/main/middleware.ts
+++ apps/darkside/main/middleware.ts
@@ -60,24 +60,31 @@ export default async function middleware(request: NextRequest, _event: NextFetch
 
   // exclude API and Next.js internal routes
   if (!url.pathname.startsWith('/api') && !url.pathname.startsWith('/_next')) {
-    const localRedirectDestination = await kv.hget<string>('redirects', url.pathname);
-    const localRedirectSourceWithQuery = url.pathname + url.search;
-
+    let localRedirectDestination = await kv.hget<string>('redirects', url.pathname);
+    
     if (localRedirectDestination) {
+      console.log("Found redirect", localRedirectDestination)
+      // If its a PDP, try to get more specific redirect
+      if (url.pathname.startsWith('/engagement-ring') || url.pathname.startsWith('/jewelry')){
+        console.log("PDP", { path: url.pathname, search: url.search})
+        // First reduce search to known values and order 
+        const reducedSearch = url.search // TODO
+        const localRedirectSourceWithQuery = url.pathname + reducedSearch;
+        const redirectWithQuery = await kv.hget<string>('redirects', localRedirectSourceWithQuery);
+
+        if (redirectWithQuery) {
+          console.log("Found PDP url with query");
+          localRedirectDestination = redirectWithQuery;
+        } else {
+          console.log("Found PDP path only redirect");
+        }
+      }
+
       url.pathname = localRedirectDestination;
       const isPermanent = await kv.sismember('permanent_redirects', url.pathname);
 
       return NextResponse.redirect(url, isPermanent ? 301 : 302);
-    } else {
-      const localRedirectDestinationWithQuery = await kv.hget<string>('redirects', localRedirectSourceWithQuery);
-
-      if (localRedirectDestinationWithQuery) {
-        url.pathname = localRedirectDestinationWithQuery;
-        const isPermanent = await kv.sismember('permanent_redirects', url.pathname);
-
-        return NextResponse.redirect(url, isPermanent ? 301 : 302);
-      }
-    }
+    } 
 
     const localRewriteDestination = await kv.hget<string>('rewrites', url.pathname);
 
