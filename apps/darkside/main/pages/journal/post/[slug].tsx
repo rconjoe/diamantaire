5b42cd648a0fd9d165b3a6a830683e27c4f3f67e
div <samjdavidoff@gmail.com>
2024-01-14 15:47:44 -0500

    journal rendering

---
+++ apps/darkside/main/pages/journal/post/[slug].tsx
@@ -0,0 +1,59 @@
+import { queries } from '@diamantaire/darkside/data/queries';
+import { SingleJournalEntry } from '@diamantaire/darkside/page/journal';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+
+export default SingleJournalEntry;
+
+// need to figure this out
+// export async function getStaticPaths() {
+//   let paths = await getAllJournalSlugs();
+
+//   paths = paths.map((path) => {
+//     const newPath = path.replace('https://www.vrai.com/journal', '');
+
+//     return newPath;
+//   });
+
+//   const updatedPaths = paths.map((slug) => {
+//     return `/post/${slug}`;
+//   });
+
+//   return {
+//     paths: updatedPaths,
+//     fallback: false,
+//   };
+// }
+
+export async function getServerSideProps({
+  locale,
+  params,
+}: {
+  locale: string;
+  params: {
+    slug: string;
+  };
+}) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    ...queries.template.global(locale),
+  });
+
+  await queryClient.prefetchQuery({
+    ...queries.journal.journalHeader(locale),
+  });
+
+  await queryClient.prefetchQuery({
+    ...queries.journal.journalHeader(locale),
+  });
+
+  await queryClient.prefetchQuery({
+    ...queries.journal.singleJournal(locale, params.slug),
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
