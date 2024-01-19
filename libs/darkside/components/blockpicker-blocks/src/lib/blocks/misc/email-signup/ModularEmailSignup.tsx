669db61d3c2162e560435a6c9e0d67d62d305147
Dan Au <buzzguy@gmail.com>
2024-01-19 17:10:56 -0500

    refactor modular block email formi

--- libs/darkside/components/blockpicker-blocks/src/lib/blocks/misc/email-signup/ModularEmailSignup.tsx
+++ libs/darkside/components/blockpicker-blocks/src/lib/blocks/misc/email-signup/ModularEmailSignup.tsx
@@ -1,8 +1,12 @@
-import { Form, FormSchemaType, Heading } from '@diamantaire/darkside/components/common-ui';
+import { Form, FormSchemaType, Heading, Loader } from '@diamantaire/darkside/components/common-ui';
+import { sendHubspotForm } from '@diamantaire/darkside/data/api';
 import { useTranslations } from '@diamantaire/darkside/data/hooks';
-import { isCountrySupported } from '@diamantaire/shared/helpers';
+import { HUBSPOT_CONTENT_BLOCK_LIST } from '@diamantaire/shared/constants';
+import { getIsUserInUs } from '@diamantaire/shared/geolocation';
+import { isCountrySupported, getUserCountry } from '@diamantaire/shared/helpers';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import { ModularEmailSignupBlockContainer } from './ModularEmailSignup.style';
 
@@ -20,22 +24,59 @@ type ModularEmailSignupBlockProps = {
   supportedCountries: Array<{
     code: string;
   }>;
+  ctaCopy?: string;
+  optInCopy?: string;
 };
 
 const ModularEmailSignupBlock = ({
   title,
   copy,
-  //   listData,
-  //   ctaCopy,
+  listData = HUBSPOT_CONTENT_BLOCK_LIST,
+  ctaCopy,
+  //optInCopy = 'Opt me in',
   // enablePhoneFieldTitle,
   //   enableStackedView,
   enablePhoneField,
   supportedCountries,
-  countryCode,
+  // countryCode,
   additionalClass,
   headingType,
   headingAdditionalClass,
 }: ModularEmailSignupBlockProps) => {
+  const [loading, setLoading] = useState(false);
+  const [showOptIn, setShowOptIn] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [isValid, setIsValid] = useState(true);
+  const countryCode = getUserCountry();
+
+  useEffect(() => {
+    const isUserInUs = getIsUserInUs();
+
+    setShowOptIn(!isUserInUs);
+    setLoading(false);
+  }, []);
+
+  const onSubmit = async (e, formState) => {
+    e.preventDefault();
+
+    const { email, isConsent } = formState;
+
+    if (showOptIn && !isConsent) {
+      setIsValid(false);
+
+      return;
+    }
+
+    try {
+      if (!showOptIn || (showOptIn && isConsent)) {
+        const response = await sendHubspotForm({ email, listData, isConsent, countryCode, locale });
+
+        setMessage(response.inlineMessage);
+      }
+    } catch (error) {
+      console.error('Error submitting form data to HubSpot:', error);
+    }
+  };
   const { locale } = useRouter();
   const { _t } = useTranslations(locale);
 
@@ -43,7 +84,8 @@ const ModularEmailSignupBlock = ({
   if (!isCountrySupported(supportedCountries, countryCode)) {
     return null;
   }
-
+  const lowercaseUserCountryCode = countryCode?.toLowerCase();
+  const optInCopy = _t('optInCopy');
   const formSchema: FormSchemaType[] = [
     {
       name: 'email',
@@ -59,7 +101,13 @@ const ModularEmailSignupBlock = ({
       inputType: 'phone',
       defaultValue: '',
       placeholder: _t('Enter your phone number'),
+      defaultCountry: lowercaseUserCountryCode,
+      required: false,
     });
+  }
+
+  if (loading) {
+    return <Loader />;
   }
 
   return (
@@ -79,15 +127,31 @@ const ModularEmailSignupBlock = ({
       </div>
       <div className="email-signup__form-container">
         <div className="email-signup__form-wrapper">
-          <Form
-            flexDirection={formSchema?.length > 1 ? 'column' : 'row'}
-            schema={formSchema}
-            onSubmit={(e) => e.preventDefault()}
-          />
+          {message ? (
+            <div dangerouslySetInnerHTML={{ __html: message }}></div>
+          ) : (
+            <Form
+              formGridStyle="single"
+              flexDirection="column"
+              schema={formSchema}
+              onSubmit={onSubmit}
+              showOptIn={showOptIn}
+              ctaCopy={ctaCopy}
+              optInCopy={optInCopy}
+              isValid={isValid}
+              setIsValid={setIsValid}
+              extraClass="-modular-block"
+              stackedSubmit={true}
+            />
+          )}
         </div>
       </div>
     </ModularEmailSignupBlockContainer>
   );
+};
+
+ModularEmailSignupBlock.defaultProps = {
+  listData: HUBSPOT_CONTENT_BLOCK_LIST,
 };
 
 export default ModularEmailSignupBlock;
