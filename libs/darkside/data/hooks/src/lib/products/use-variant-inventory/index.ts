eb0be4b7d9897db35a829e41c3b66319496b5013
Dan Au <buzzguy@gmail.com>
2024-01-23 09:55:56 -0500

    add abort controller when users are switching back and forth between options

--- libs/darkside/data/hooks/src/lib/products/use-variant-inventory/index.ts
+++ libs/darkside/data/hooks/src/lib/products/use-variant-inventory/index.ts
@@ -1,46 +1,53 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export function useVariantInventory(variantId: string, trackInventory: boolean) {
   const [isInStock, setInStock] = useState(true);
   const [isFetching, setIsFetching] = useState(false);
-
-  // Extract numerical ID if variantId is in a URL format
-  let numericalVariantId = variantId.includes('gid') ? variantId.split('/').pop() : variantId;
+  const abortController = useRef(new AbortController());
 
   useEffect(() => {
     const fetchStockByVariant = async (id: string) => {
       if (trackInventory) {
         setIsFetching(true);
-
-        // Constructing the request URL
-        const BASE_URL =
-          typeof window === 'undefined'
-            ? process.env['NEXT_PUBLIC_PROTOCOL'] + process.env['NEXT_PUBLIC_VERCEL_URL']
-            : window.location.origin;
-        const reqUrl = `${BASE_URL}/api/products/inventory?${new URLSearchParams({ variantId: id }).toString()}`;
+        abortController.current.abort(); // Cancel the previous fetch request
+        abortController.current = new AbortController();
 
         try {
-          const response = await fetch(reqUrl);
+          const BASE_URL =
+            typeof window === 'undefined'
+              ? process.env['NEXT_PUBLIC_PROTOCOL'] + process.env['NEXT_PUBLIC_VERCEL_URL']
+              : window.location.origin;
+          const reqUrl = `${BASE_URL}/api/products/inventory?${new URLSearchParams({ variantId: id }).toString()}`;
+
+          const response = await fetch(reqUrl, { signal: abortController.current.signal });
           const { inventoryQuantity, inventoryPolicy } = await response.json();
 
-          // Determine stock status based on inventory policy and quantity
           setInStock(inventoryPolicy === 'continue' || inventoryQuantity > 0);
         } catch (error) {
-          console.error('Error fetching inventory:', error);
-          setInStock(true); // Assume in stock if there's an error
+          if (error instanceof Error && error.name !== 'AbortError') {
+            console.error('Error fetching inventory:', error);
+            setInStock(true); // Assume in stock if there's an error
+          }
         } finally {
           setIsFetching(false);
         }
       } else {
-        // If not tracking inventory, assume in stock
         setInStock(true);
       }
     };
 
+    // Extract numerical ID if variantId is in a URL format
+    const numericalVariantId = variantId.includes('gid') ? variantId.split('/').pop() : variantId;
+
     // Reset isInStock based on trackInventory when variantId changes
     setInStock(!trackInventory);
     fetchStockByVariant(numericalVariantId);
-  }, [numericalVariantId, trackInventory]);
+
+    // Cleanup function to abort fetch when component unmounts or variant changes
+    return () => {
+      abortController.current.abort();
+    };
+  }, [variantId, trackInventory]);
 
   return { isFetching, isInStock };
 }
