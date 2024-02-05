1262ffe221a1af622f47041dee039c272ea3d265
charles olivier <chla84@yahoo.fr>
2024-02-05 18:15:43 +0100

    feat: tooltip bugfix

--- libs/darkside/components/common-ui/src/lib/molecules/Tooltip.tsx
+++ libs/darkside/components/common-ui/src/lib/molecules/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useRef, useState } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import { Tooltip as ReactToolTip } from 'react-tooltip';
 
 import StyledTooltip from './Tooltip.style';
@@ -17,7 +17,9 @@ const Tooltip: React.FC<TooltipProps> = ({ id, children, className, place }) => 
   const tooltipRef = useRef(null);
 
   const handleClick = (e) => {
-    if (e.relatedTarget !== tooltipRef.current) {
+    if (!tooltipRef.current || !tooltipRef.current.contains(e.target)) {
+      e.stopPropagation();
+
       setIsOpen(false);
 
       window.removeEventListener('click', handleClick);
@@ -25,45 +27,63 @@ const Tooltip: React.FC<TooltipProps> = ({ id, children, className, place }) => 
   };
 
   const handleToggleTooltip = () => {
-    setIsOpen(!isOpen);
+    const _isOpen = !isOpen;
+
+    setIsOpen(_isOpen);
+
+    if (_isOpen) window.addEventListener('click', handleClick);
   };
 
-  const handleCloseTooltip = () => {
+  const handleCloseTooltip = (e) => {
+    e.stopPropagation();
+
     setIsOpen(false);
+
+    window.removeEventListener('click', handleClick);
   };
 
-  const handleOpenTooltip = () => {
+  const handleOpenTooltip = (e) => {
+    e.stopPropagation();
+
     setIsOpen(true);
 
     window.addEventListener('click', handleClick);
   };
 
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('click', handleClick);
+    };
+  }, []);
+
   return (
-    <StyledTooltip ref={tooltipRef}>
-      <button
-        data-tooltip-id={id}
-        className="tooltip-trigger tablet-and-up"
-        onMouseEnter={handleOpenTooltip}
-        onMouseLeave={handleCloseTooltip}
-        onClick={handleToggleTooltip}
-      >
-        <span>i</span>
-      </button>
+    <StyledTooltip>
+      <div ref={tooltipRef}>
+        <button
+          data-tooltip-id={id}
+          className="tooltip-trigger tablet-and-up"
+          onMouseEnter={handleOpenTooltip}
+          onMouseLeave={handleCloseTooltip}
+          onClick={handleToggleTooltip}
+        >
+          <span>i</span>
+        </button>
 
-      <button data-tooltip-id={id} className="tooltip-trigger mobile-only" onClick={handleToggleTooltip}>
-        <span>i</span>
-      </button>
+        <button data-tooltip-id={id} className="tooltip-trigger mobile-only" onClick={handleToggleTooltip}>
+          <span>i</span>
+        </button>
 
-      <ReactToolTip
-        id={id}
-        place={place}
-        className={`tooltip-window ${className}`}
-        classNameArrow="tooltip-arrow"
-        data-tooltip-variant="light"
-        isOpen={isOpen}
-      >
-        {children}
-      </ReactToolTip>
+        <ReactToolTip
+          id={id}
+          place={place}
+          className={`tooltip-window ${className}`}
+          classNameArrow="tooltip-arrow"
+          data-tooltip-variant="light"
+          isOpen={isOpen}
+        >
+          {children}
+        </ReactToolTip>
+      </div>
     </StyledTooltip>
   );
 };
