b36ed62bfe7ff7568aff9f9cc3aa5bdd9c179b5e
div <samjdavidoff@gmail.com>
2024-02-07 15:40:34 -0500

    more fix

--- apps/darkside/main/pages/customize/diamond-to-setting/[lotId]/[collectionSlug]/[productSlug]/summary/index.tsx
+++ apps/darkside/main/pages/customize/diamond-to-setting/[lotId]/[collectionSlug]/[productSlug]/summary/index.tsx
@@ -672,6 +672,7 @@ const ReviewBuildStep = ({ settingSlugs }) => {
       const diamondAttributes: ProductAddonDiamond['attributes'] = {
         _productTitle: diamond?.productTitle,
         productAsset: diamondImages[index],
+        _productAssetObject: '',
         _dateAdded: (Date.now() + 100).toString(),
         caratWeight: diamond.carat.toString(),
         clarity: diamond.clarity,
