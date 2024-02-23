ae30931a7cd9428681859ee670a02e717ebc60f6
Dan Au <buzzguy@gmail.com>
2024-02-23 14:22:46 -0500

    add script mt bold font
    send _EngravingFont attrib

--- libs/styles/darkside-styles/src/lib/fonts.ts
+++ libs/styles/darkside-styles/src/lib/fonts.ts
@@ -9,7 +9,13 @@ export const DEFAULT_SINGLE_ENGRAVING_FONT = ENGRAVING_FONTS[3];
 export const ENGRAVING_FONT_RENDER_MAP = {
   Serif: 'Times New Roman',
   'Sans Serif': 'Lucida Sans Demibold Roman',
-  Script: 'Script MT Bold',
+  Script: 'Script MT Bold', //'Script MT Bold'
+  Monogram: 'Monogram',
+};
+export const ENGRAVING_FONT_INPUT_RENDER_MAP = {
+  Serif: 'Times New Roman',
+  'Sans Serif': 'Lucida Sans Demibold Roman',
+  Script: '--font-family-script-mt', //'Script MT Bold'
   Monogram: 'Monogram',
 };
 export const HOME_PAGE_P_FONT_SIZE = '1.8rem';
@@ -53,10 +59,21 @@ export const getRenderedEngravingFontStylesForCart = (engravingRenderedFont) => 
 
 export const getRenderedInputEngravingFontStyles = (selectedEngravingFont) => {
   return `
-    font-family: '${ENGRAVING_FONT_RENDER_MAP[selectedEngravingFont]}';
+    font-family: ${getFontFamily(ENGRAVING_FONT_INPUT_RENDER_MAP[selectedEngravingFont])};
     font-size: ${ENGRAVING_FONT_SIZE_MAP_MOBILE[selectedEngravingFont]};
     ${tabletAndUp(`
       font-size: ${ENGRAVING_FONT_SIZE_MAP[selectedEngravingFont]};
     `)};
   `;
 };
+
+function getFontFamily(font) {
+  // Check if the font is a CSS variable
+  if (font.startsWith('--')) {
+    // If it is, use var()
+    return `var(${font})`;
+  } else {
+    // If it's not, return the font name directly
+    return font;
+  }
+}
