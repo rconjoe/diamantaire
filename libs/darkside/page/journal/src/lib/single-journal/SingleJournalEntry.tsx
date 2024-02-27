5306c5a4c89131c30174ae69ebbc8dd2aa49ad57
Carlos Gualan Llanos <carlosgualan@vrai.com>
2024-02-27 15:20:28 -0500

    fix DIA-260: Journal posts do not show author's name and published date

--- libs/darkside/page/journal/src/lib/single-journal/SingleJournalEntry.tsx
+++ libs/darkside/page/journal/src/lib/single-journal/SingleJournalEntry.tsx
@@ -8,6 +8,7 @@ import { NextSeo } from 'next-seo';
 import React, { useEffect, useMemo, useState } from 'react';
 
 import { SingleJournalEntryStyles } from './SingleJourneyEntry.style';
+import { generateSubheading } from '../journal-helpers';
 
 const SingleJournalEntry = () => {
   const router = useRouter();
@@ -71,9 +72,12 @@ const SingleJournalEntry = () => {
       )}
 
       <div className="journal-entry__crumbs">{crumbs && <Breadcrumb breadcrumb={crumbs} simple={true} />}</div>
-
       {data?.blogPost?.content?.map((contentBlockData) => {
         const { id, _modelApiKey } = contentBlockData;
+
+        if (contentBlockData?.title) {
+          contentBlockData.subtitle = generateSubheading(data?.blogPost)
+        }
 
         return (
           <React.Fragment key={id}>
