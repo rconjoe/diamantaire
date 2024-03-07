1ceacde50dcd3b5ebb8ea129388a5a818f346551
charles olivier <chla84@yahoo.fr>
2024-03-07 19:37:49 +0100

    feat: console

--- libs/darkside/page/diamonds/src/lib/CFYResultPage.tsx
+++ libs/darkside/page/diamonds/src/lib/CFYResultPage.tsx
@@ -292,6 +292,8 @@ const CFYResultPage = (props: InferGetServerSidePropsType<typeof getServerSidePr
 
   const { data: { productType = '' } = {} } = productQuery || {};
 
+  console.log(`productQuery`, productQuery);
+
   const appointmentEl = useRef(null);
 
   const getToAppointment = (e) => {
