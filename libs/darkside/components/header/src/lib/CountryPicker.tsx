e97022264566c5a98f703ed4408ce0979d86142e
Dan Au <buzzguy@gmail.com>
2024-03-13 10:34:34 -0400

    add an animation dropdown

--- libs/darkside/components/header/src/lib/CountryPicker.tsx
+++ libs/darkside/components/header/src/lib/CountryPicker.tsx
@@ -1,4 +1,5 @@
 import { CountrySelectorStyles, Heading } from '@diamantaire/darkside/components/common-ui';
+import { motion } from 'framer-motion';
 import {
   getLocaleFromCountry,
   getCountryName,
@@ -14,8 +15,12 @@ import { useRouter } from 'next/router';
 import { useState, useEffect, useMemo } from 'react';
 import styled from 'styled-components';
 
-const BannerWrapper = styled.div`
-  position: relative;
+const BannerWrapper = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 6000;
 `;
 
 const BarStyles = styled.div`
@@ -168,7 +173,7 @@ function CountryPicker() {
   if (!isBannerVisible) return null;
 
   return (
-    <BannerWrapper>
+    <BannerWrapper initial={{ y: -100 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120, damping: 10 }}>
       <BarStyles>
         <div className="buttons-wrapper">
           <ShopItButton onClick={handleRecommendedCountryChange}>
