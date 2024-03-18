cb0f195d6ecd3c315d9f3e3f823bc527f65a8123
div <samjdavidoff@gmail.com>
2024-03-18 18:55:08 -0400

    fix redirect from sign up page

--- apps/darkside/main/pages/account/sign-up.tsx
+++ apps/darkside/main/pages/account/sign-up.tsx
@@ -1,51 +1,10 @@
-import { SignUp } from '@clerk/nextjs';
-import { useTranslations } from '@diamantaire/darkside/data/hooks';
-import { POST_SIGN_IN_REDIRECT_URL, POST_SIGN_UP_REDIRECT_URL } from '@diamantaire/darkside/page/accounts';
 import { getTemplate as getAccountTemplate } from '@diamantaire/darkside/template/accounts';
 import { useRouter } from 'next/router';
-import { NextSeo } from 'next-seo';
-import styled from 'styled-components';
 
-const SignUpPageStyles = styled.div`
-  padding: 50px 0 100px;
-  margin: 0 auto;
-  max-width: 400px;
-  min-height: 5.5rem;
-
-  .cl-rootBox {
-    width: 100%;
-    margin: 0 auto;
-    .cl-card {
-      width: 100%;
-      margin: 0px;
-      .cl-formFieldInput {
-        padding-left: 15px;
-      }
-    }
-  }
-`;
 const SignInPage = () => {
-  const { locale } = useRouter();
-  const { _t } = useTranslations(locale);
+  const router = useRouter();
 
-  return (
-    <SignUpPageStyles>
-      <NextSeo title={`${_t('Sign Up')} | VRAI`} />
-      <SignUp
-        appearance={{
-          variables: {
-            colorPrimary: '#5e7a7d',
-            fontSize: '2rem',
-            spacingUnit: '2rem',
-          },
-        }}
-        afterSignUpUrl={POST_SIGN_UP_REDIRECT_URL}
-        afterSignInUrl={POST_SIGN_IN_REDIRECT_URL}
-        signInUrl="/account/sign-in"
-        routing="hash"
-      />
-    </SignUpPageStyles>
-  );
+  return router.push('/account/sign-in');
 };
 
 SignInPage.getTemplate = getAccountTemplate;
