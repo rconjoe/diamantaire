7e2b9f06e7280a17c420e5176575b4c3b9f9dc7b
div <samjdavidoff@gmail.com>
2024-03-18 11:46:34 -0400

    fix default export

--- apps/darkside/main/pages/sso-callback.tsx
+++ apps/darkside/main/pages/sso-callback.tsx
@@ -1,7 +1,7 @@
 import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
 
 export default function SSOCallback() {
-  // CLERK
+  // CLERK - Customer Account SSO Callback
   // Handle the redirect flow by rendering the
   // prebuilt AuthenticateWithRedirectCallback component.
   // This is the final step in the custom OAuth flow
