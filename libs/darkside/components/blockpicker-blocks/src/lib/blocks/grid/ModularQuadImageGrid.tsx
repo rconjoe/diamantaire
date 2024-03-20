eb4123d4b4d1da619f21fba80427dfd09df7a31a
Dan Au <buzzguy@gmail.com>
2024-03-20 16:27:02 -0400

    revert mod quad image grid

--- libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularQuadImageGrid.tsx
+++ libs/darkside/components/blockpicker-blocks/src/lib/blocks/grid/ModularQuadImageGrid.tsx
@@ -21,6 +21,7 @@ type ModularGridCarouselBlockProps = {
   isMobile?: boolean;
   headingAdditionalClass?: string;
   headingType?: string;
+  shouldLazyLoad?: boolean;
   _modelApiKey?: string;
 };
 
@@ -31,6 +32,7 @@ const ModularQuadImageGrid = ({
   subtitle,
   blocks,
   _modelApiKey,
+  shouldLazyLoad,
 }: ModularGridCarouselBlockProps) => {
   const { locale } = useRouter();
   const { countryCode } = parseValidLocale(locale);
@@ -55,7 +57,7 @@ const ModularQuadImageGrid = ({
             <div className="item__container" key={id}>
               <div className="item__image">
                 <Link href={link}>
-                  <DatoImage image={desktopImage} overrideAlt={title || ''} />
+                  <DatoImage image={desktopImage} shouldLazyLoad={shouldLazyLoad} overrideAlt={title || ''} />
                 </Link>
               </div>
               <div className="item__title text-center">
