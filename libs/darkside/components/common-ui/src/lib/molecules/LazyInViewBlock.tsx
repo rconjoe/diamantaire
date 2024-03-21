b1c3ae1f376c93fdfc7a9789d19a7a81351001d6
Dan Au <buzzguy@gmail.com>
2024-03-21 16:49:45 -0400

    make rootMargin larger so we dont get the pop fouc

--- libs/darkside/components/common-ui/src/lib/molecules/LazyInViewBlock.tsx
+++ libs/darkside/components/common-ui/src/lib/molecules/LazyInViewBlock.tsx
@@ -5,7 +5,7 @@ interface LazyInViewBlockProps extends IntersectionOptions {
   children: ReactNode;
 }
 
-const LazyInViewBlock: React.FC<LazyInViewBlockProps> = ({ children, rootMargin = '200px 0px', ...props }) => (
+const LazyInViewBlock: React.FC<LazyInViewBlockProps> = ({ children, rootMargin = '500px 0px', ...props }) => (
   <InView triggerOnce={true} rootMargin={rootMargin} {...props}>
     {({ inView, ref }) => <div ref={ref}>{inView && children}</div>}
   </InView>
